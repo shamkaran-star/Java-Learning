7834e1afe05de5fc17094b29f4cc14d47b1cc3d5
shamkaran-star <rkshamj@gmail.com>
2025-08-06 17:05:54 +0530

    third commit

--- Family.java
+++ Family.java
@@ -1,6 +1,6 @@
 public class Family{
 public static void main (String[]args){
-System.Out.println("sister name :paru");
+System.Out.println("sister name :paravathi");
 System.Out.println("brother name : ram");
 System.Out.println("friend name : ajay");
   }
